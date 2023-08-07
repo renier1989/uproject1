d22c5ff94bc4f97b35ea26a84efcb27ced9da53b
Renier Vargas <renier1989@gmail.com>
2023-08-07 09:43:57 -0500

    Class43 - Dando estilos a los productos

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,15 +1,27 @@
 import { Component } from "react";
 
+const styles ={
+    producto :{
+        border: 'solid 1px #aaa',
+        boxShadow : '0 5px 5px rgba(0, 0, 0, 0.1)',
+        width : '30%',
+        padding : '10px 15px',
+        borderRadius : '5px'
+    },
+    img :{
+        width : '100%',
+    }
+}
 class  Producto extends Component {
     render(){
         console.log(this.props);
         const {producto,agregarAlCarrito } = this.props;
         
         return(
-            <div>
+            <div style={styles.producto}>
+                <img style={styles.img} src={producto.img} alt={producto.name} />
                 <h3>{producto.name} </h3>
                 <p>{producto.price}</p>
-                <img src={producto.img} alt={producto.name} />
                 <button onClick={agregarAlCarrito}>Agregar</button>
             </div>
         );
--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,10 +1,19 @@
 import { Component } from "react";
 import Producto from "./Producto";
+
+const styles = {
+    productos:{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+}
+
 class Productos extends Component {
   render() {
     const { productos, agregarAlCarrito } = this.props;
     return (
-      <div>
+      <div style={styles.productos}>
         {productos.map((producto, index) => (
           <Producto 
           key={index}
