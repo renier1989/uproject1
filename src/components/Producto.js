4f1946558f6f9544b84e17e577d732bd817f8ffa
Renier Vargas <renier1989@gmail.com>
2023-08-07 07:12:43 -0500

    Class42 _ min fix

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -3,12 +3,14 @@ import { Component } from "react";
 class  Producto extends Component {
     render(){
         console.log(this.props);
-        const {producto } = this.props;
+        const {producto,agregarAlCarrito } = this.props;
+        
         return(
             <div>
                 <h3>{producto.name} </h3>
                 <p>{producto.price}</p>
                 <img src={producto.img} alt={producto.name} />
+                <button onClick={agregarAlCarrito}>Agregar</button>
             </div>
         );
     }
